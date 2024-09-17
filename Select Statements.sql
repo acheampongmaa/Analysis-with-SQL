1241f56a5e19adf7563de98b77744cd84f69d23d
Queensly K. Acheampongmaa <115668686+acheampongmaa@users.noreply.github.com>
2024-09-17 17:26:28 +0000

    Added 'Select' statements

---
+++ Select Statements.sql
@@ -0,0 +1,33 @@
+/* Select Statement
+ *, Top, Distinct, Count, AS, MIN, Max, AVG
+ */
+ 
+ --Using Top
+--SELECT TOP 5 *
+--FROM EmployeeDemographics
+
+--Using Distinct to select unique values
+--SELECT DISTINCT (employeeid)
+--FROM EmployeeDemographics
+
+--SELECT DISTINCT (gender)
+--FROM EmployeeDemographics
+ 
+ -- Using Count
+ --SELECT COUNT (lastname)
+--FROM EmployeeDemographics
+
+-- Using AS to name the table that results from the COUNT
+--SELECT COUNT (lastname) AS LastNameCount
+--FROM EmployeeDemographics
+ 
+--Using Max, Min and AVG
+
+--SELECT MAX (salary) AS MaxSalary
+--FROM EmployeeSalary
+
+--SELECT Min (salary) AS MinSalary
+--FROM EmployeeSalary
+
+--SELECT AVG (salary) AS AvgSalary
+--FROM EmployeeSalary
