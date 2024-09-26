4ecd3ba0ccce0d88ab09665b051424f0a61287b4
Queensly K. Acheampongmaa <115668686+acheampongmaa@users.noreply.github.com>
2024-09-26 12:03:49 +0000

    Added 'Group and Order By' Statements

---
+++ Group and Order By Statements.sql
@@ -0,0 +1,46 @@
+/* Group By, Order By statement
+ */
+ 
+ /*Group by is used to apply some sort of aggregate fxn in each group
+ If there are male and female in the dataset, using group by will result
+ in having the distinct names. When count is included, it counts the number of appearance
+ in each group
+ */
+ --Example 1
+ --SELECT Gender, COUNT(Gender)
+ --FROM EmployeeDemographics
+ --GROUP BY Gender
+ 
+ --Example 2
+ --SELECT Gender, Age, COUNT(Gender)
+ --FROM EmployeeDemographics
+ --GROUP BY Gender, Age
+ 
+ --Example 3
+ --SELECT Gender,Age, COUNT(Gender)
+ --FROM EmployeeDemographics
+ --WHERE Age > 30
+-- GROUP BY Gender, Age
+ 
+ /* Order by. This is used to order the count. SQL has the default of ASC
+ */
+ --Example 1
+ --SELECT Gender,Age, COUNT(Gender) As CountGender
+-- FROM EmployeeDemographics
+ --GROUP BY Gender, Age
+ --ORDER BY CountGender DESC
+ 
+ --Example 2
+ --SELECT *
+ --FROM EmployeeDemographics
+ --ORDER BY Age
+ 
+ --Example 3
+ --SELECT *
+ --FROM EmployeeDemographics
+ --ORDER BY Age DESC, Gender DESC
+ 
+ --Example 4 One can use the column number instead of name
+ SELECT *
+ FROM EmployeeDemographics
+ ORDER BY 4 DESC, 5 DESC
