083356bbf658a61d2d3365fbc30edc5f847a0637
Queensly K. Acheampongmaa <115668686+acheampongmaa@users.noreply.github.com>
2024-10-03 11:07:44 +0000

    Update Select Statements.sql

--- Select Statements.sql
+++ Select Statements.sql
@@ -2,32 +2,32 @@
  *, Top, Distinct, Count, AS, MIN, Max, AVG
  */
  
- --Using Top
---SELECT TOP 5 *
---FROM EmployeeDemographics
+ /* Using Top */
+SELECT TOP 5 *
+FROM EmployeeDemographics
 
---Using Distinct to select unique values
---SELECT DISTINCT (employeeid)
---FROM EmployeeDemographics
+/* Using Distinct to select unique values */
+SELECT DISTINCT (employeeid)
+FROM EmployeeDemographics
 
---SELECT DISTINCT (gender)
---FROM EmployeeDemographics
+SELECT DISTINCT (gender)
+FROM EmployeeDemographics
  
- -- Using Count
- --SELECT COUNT (lastname)
---FROM EmployeeDemographics
+ /* Using Count */
+ SELECT COUNT (lastname)
+FROM EmployeeDemographics
 
--- Using AS to name the table that results from the COUNT
---SELECT COUNT (lastname) AS LastNameCount
---FROM EmployeeDemographics
+/* Using AS to name the table that results from the COUNT */
+SELECT COUNT (lastname) AS LastNameCount
+FROM EmployeeDemographics
  
---Using Max, Min and AVG
+/* Using Max, Min and AVG */
 
---SELECT MAX (salary) AS MaxSalary
---FROM EmployeeSalary
+SELECT MAX (salary) AS MaxSalary
+FROM EmployeeSalary
 
---SELECT Min (salary) AS MinSalary
---FROM EmployeeSalary
+SELECT Min (salary) AS MinSalary
+FROM EmployeeSalary
 
---SELECT AVG (salary) AS AvgSalary
---FROM EmployeeSalary
+SELECT AVG (salary) AS AvgSalary
+FROM EmployeeSalary
