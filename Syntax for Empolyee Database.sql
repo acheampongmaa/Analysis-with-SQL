92365d93c6a810544e6163322016ebfba855b25f
Queensly K. Acheampongmaa <115668686+acheampongmaa@users.noreply.github.com>
2024-10-03 11:05:15 +0000

    Update Syntax for Empolyee Database.sql

--- Syntax for Empolyee Database.sql
+++ Syntax for Empolyee Database.sql
@@ -1,51 +1,53 @@
---Creating employee table. First is the demographics, second the salary table.
---CREATE TABLE EmployeeDemographics 
---(EmployeeID int,
- --FirstName varchar(50),
- --LastName varchar(50),
- --Age int,
- --Gender varchar(50)
- --)
+/* Creating employee table. First is the demographics, and second is the salary table. */
+CREATE TABLE EmployeeDemographics 
+(EmployeeID int,
+ FirstName varchar(50),
+ LastName varchar(50),
+ Age int,
+ Gender varchar(50)
+ )
 
  
- --CREATE TABLE EmployeeSalary
- --(EmployeeID int,
-  --JobTitle varchar (50),
-  --salary int
-  --)
+ CREATE TABLE EmployeeSalary
+ (EmployeeID int,
+  JobTitle varchar (50),
+  salary int
+  )
   
-  -- Inserting values into the tables
+  /*  Inserting values into the tables */
   
- --INSERT INTO EmployeeDemographics VALUES
-  --(1001, 'Ama', 'Kwakye', 25, 'Female'),
-  --(1002, 'Kofi', 'Mensah', 32, 'Male'),
-  --(1003, 'Araba', 'Kyei', 30, 'Female'),
-  --(1004, 'Kwabena', 'Bonsu', 29, 'Male'),
-  --(1005, 'Yaw', 'Twum', 31, 'Male'),
-  --(1006, 'Esi', 'Thompson', 38, 'Female'),
-  --(1007, 'Kojo', 'Benyin', 35, 'Male'),
-  --(1008, 'Kwesi', 'Banfo', 32, 'Male'),
-  --(1009, 'Afua', 'Gyakye', 28, 'Female'),
-  --(1010, 'Adoma', 'Sagoe', 36, 'Female')
+ INSERT INTO EmployeeDemographics VALUES
+  (1001, 'Ama', 'Kwakye', 25, 'Female'),
+  (1002, 'Kofi', 'Mensah', 32, 'Male'),
+  (1003, 'Araba', 'Kyei', 30, 'Female'),
+  (1004, 'Kwabena', 'Bonsu', 29, 'Male'),
+  (1005, 'Yaw', 'Twum', 31, 'Male'),
+  (1006, 'Esi', 'Thompson', 38, 'Female'),
+  (1007, 'Kojo', 'Benyin', 35, 'Male'),
+  (1008, 'Kwesi', 'Banfo', 32, 'Male'),
+  (1009, 'Afua', 'Gyakye', 28, 'Female'),
+  (1010, 'Adoma', 'Sagoe', 36, 'Female')
   
   
-  --INSERT INTO EmployeeSalary VALUES
-  --(1001, 'Sales Personnel', 45000),
-  --(1002, 'Receptionist', 36000),
-  --(1003, 'Accountant', 63000),
-  --(1004, 'HR', 47000),
-  --(1005, 'Regional Manager', 65000),
-  --(1006, 'HR Assistant', 38000),
-  --(1007, 'Sales Personnel', 45000),
-  --(1008, 'Driver', 32000),
-  --(1009, 'Supplier Relations', 41000),
-  --(1010, 'Shop Manager', 60000)
+  INSERT INTO EmployeeSalary VALUES
+  (1001, 'Sales Personnel', 45000),
+  (1002, 'Receptionist', 36000),
+  (1003, 'Accountant', 63000),
+  (1004, 'HR', 47000),
+  (1005, 'Regional Manager', 65000),
+  (1006, 'HR Assistant', 38000),
+  (1007, 'Sales Personnel', 45000),
+  (1008, 'Driver', 32000),
+  (1009, 'Supplier Relations', 41000),
+  (1010, 'Shop Manager', 60000)
   
-  --Viewing the values in the table
+   /* Viewing the values in the table */
   
-  --SELECT *
-  --FROM EmployeeSalary
-  
+  SELECT *
+  FROM EmployeeSalary
+
+SELECT *
+  FROM EmployeeDemographics
  
  
  
