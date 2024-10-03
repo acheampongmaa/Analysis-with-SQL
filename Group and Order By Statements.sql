0bce3ba4eaff15a227338109fbd55dfd24d45cae
Queensly K. Acheampongmaa <115668686+acheampongmaa@users.noreply.github.com>
2024-10-03 11:16:50 +0000

    Update Group and Order By Statements.sql

--- Group and Order By Statements.sql
+++ Group and Order By Statements.sql
@@ -1,46 +1,48 @@
 /* Group By, Order By statement
  */
  
- /*Group by is used to apply some sort of aggregate fxn in each group
- If there are male and female in the dataset, using group by will result
- in having the distinct names. When count is included, it counts the number of appearance
+ /*Group by is used to apply some aggregate function in each group
+ If males and females are in the dataset, using 'group by' will result
+ in distinct names. When count is included, it counts the number of appearances
  in each group
  */
- --Example 1
- --SELECT Gender, COUNT(Gender)
- --FROM EmployeeDemographics
- --GROUP BY Gender
+
+ /* Example 1 */
+ SELECT Gender, COUNT(Gender)
+ FROM EmployeeDemographics
+ GROUP BY Gender
  
- --Example 2
- --SELECT Gender, Age, COUNT(Gender)
- --FROM EmployeeDemographics
- --GROUP BY Gender, Age
+/* Example 2 */
+ SELECT Gender, Age, COUNT(Gender)
+ FROM EmployeeDemographics
+ GROUP BY Gender, Age
  
- --Example 3
- --SELECT Gender,Age, COUNT(Gender)
- --FROM EmployeeDemographics
- --WHERE Age > 30
--- GROUP BY Gender, Age
+/* Example 3 */
+ SELECT Gender,Age, COUNT(Gender)
+ FROM EmployeeDemographics
+ WHERE Age > 30
+GROUP BY Gender, Age
  
  /* Order by. This is used to order the count. SQL has the default of ASC
  */
- --Example 1
- --SELECT Gender,Age, COUNT(Gender) As CountGender
--- FROM EmployeeDemographics
- --GROUP BY Gender, Age
- --ORDER BY CountGender DESC
+  
+ /* Example 1 */
+ SELECT Gender,Age, COUNT(Gender) As CountGender
+FROM EmployeeDemographics
+GROUP BY Gender, Age
+ORDER BY CountGender DESC
  
- --Example 2
- --SELECT *
- --FROM EmployeeDemographics
- --ORDER BY Age
+ /* Example 2 */
+ SELECT *
+ FROM EmployeeDemographics
+ ORDER BY Age
  
- --Example 3
- --SELECT *
- --FROM EmployeeDemographics
- --ORDER BY Age DESC, Gender DESC
+ /* Example 3 */
+ SELECT *
+ FROM EmployeeDemographics
+ ORDER BY Age DESC, Gender DESC
  
- --Example 4 One can use the column number instead of name
+ /* Example 4. One can use the column number instead of name */
  SELECT *
  FROM EmployeeDemographics
  ORDER BY 4 DESC, 5 DESC
